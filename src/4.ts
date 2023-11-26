77f2c48241ebba3eb313ee05c70d373ba03e656d
Anna Grigoreva <101868546+AlienTuna@users.noreply.github.com>
2023-11-26 23:35:07 +0300

    TSGI-3

---
+++ src/4.ts
@@ -0,0 +1,52 @@
+class Key {
+    private signature: string;
+    constructor() {
+        this.signature = `${Math.random()}`
+    }
+
+    getSignature() {
+        return this.signature;
+    }
+}
+class Person {
+    private key: Key;
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    getKey(): Key {
+        return this.key;
+    }
+}
+class House {
+    door: boolean = false;
+    key: Key;
+    tenants: Person[] = [];
+    constructor(key: Key) {}
+
+    comeIn(newPerson: Person) {
+        if(this.door) {
+            this.tenants.push(newPerson);
+        }
+    }
+    OpenDoor(key: Key) {}
+}
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if(key === this.key) {
+            this.door = true;
+        }
+    }
+}
+
+const key = new Key();
+
+const house = new MyHouse(key);
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+
+house.comeIn(person);
+
+
+export {};
